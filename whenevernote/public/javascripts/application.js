6747b2531714561390a23ac174ae1b50adf48daa
Andreas Heim <andreas@heim.no>
2011-03-17 22:00:14 +0100

    ..

--- whenevernote/public/javascripts/application.js
+++ whenevernote/public/javascripts/application.js
@@ -5,12 +5,22 @@ var cache = {
 			function(event) {
 				$("#eventlog").append(event.type + "<br />");
 			});
+	},
+	
+	alertWhenNewCache: function(){
+		$(window.applicationCache ).bind("updateready", function(event) {
+			window.applicationCache.swapCache();
+			if(confirm("En nyere versjon av siden eksisterer, laste siden på nytt?")) {
+				window.location.reload();
+			}
+		});
 	}
 }
 
 
 $(function() {
 	cache.showCacheEvents();
+	cache.alertWhenNewCache();
 });
 
 
