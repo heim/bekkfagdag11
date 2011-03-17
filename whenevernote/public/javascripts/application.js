997522511e2f0bc656f5fcbd17d2a2415672b2cd
Andreas Heim <andreas@heim.no>
2011-03-17 22:19:34 +0100

    ..

--- whenevernote/public/javascripts/application.js
+++ whenevernote/public/javascripts/application.js
@@ -22,6 +22,41 @@ var notes = {
 		$.retrieveJSON("/notes.json", function(data) {
 			$("#notes").html($("#note_template").tmpl(data));
 		});
+	},
+	
+	sendPending: function(){
+		if (window.navigator.onLine) {
+			var pendingNotes = $.parseJSON(localStorage["pendingNotes"]);
+	    if (pendingNotes.length > 0) {
+	    	var note = pendingNotes[0];
+	      $.post("/notes", note.data, function(data) {
+	     		var pendingNotes = $.parseJSON(localStorage["pendingNotes"]);
+	        pendingNotes.shift();
+	        localStorage["pendingNotes"] = JSON.stringify(pendingNotes)
+	        setTimeout(notes.sendPending, 100);
+	      });
+			}
+		}
+	},
+	
+	initializePending: function(){
+		if (!localStorage["pendingNotes"]) {
+	    localStorage["pendingNotes"] = JSON.stringify([]);
+	  }
+	},
+	
+	overrideSubmit: function(){
+		$("#new_note").submit(function(e) {
+			e.preventDefault();
+			var pendingNotes = $.parseJSON(localStorage["pendingNotes"]);
+	    var note = {"data":$(this).serialize(), "note":{"note_text":$("#note_note_text").val()}};
+			$("#note_template").tmpl(note).appendTo("#notes");
+	    pendingNotes.push(note);
+	    localStorage["pendingNotes"] = JSON.stringify(pendingNotes)
+	    $("#note_note_text").val("");
+	    notes.sendPending();
+
+	  });
 	}
 }
 
@@ -31,7 +66,9 @@ $(function() {
 	
 	
 	notes.retrieve();
-	
+	notes.initializePending();
+	notes.overrideSubmit();
+	notes.sendPending();
 
 
 	$("#networkstatus").text(window.navigator.onLine ? "Online" : "Offline");
