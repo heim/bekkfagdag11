6670ab66b2456752e01d5b01f9b80aeb33d84614
Andreas Heim <andreas@heim.no>
2011-03-17 21:55:19 +0100

    ..

--- whenevernote/public/javascripts/application.js
+++ whenevernote/public/javascripts/application.js
@@ -1,5 +1,16 @@
-$(function() {
+var cache = {
+	showCacheEvents: function(){
+		$(window.applicationCache).bind(
+			'checking downloading progress updateready cached noupdate error', 
+			function(event) {
+				$("#eventlog").append(event.type + "<br />");
+			});
+	}
+}
 
+
+$(function() {
+	cache.showCacheEvents();
 });
 
 
